f989f2816de71158c6da418b8c286e07020c800b
Bohdan <mr.bogdan.99@gmail.com>
2022-01-30 01:51:33 +0200

    added screens

---
+++ screens/AddItemScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const AddItemScreen: React.FC<{}> = () => {
+    return (
+        <ScrollView>
+            <Text>Add Item Screen</Text>
+        </ScrollView>
+    );
+}
+
+export default AddItemScreen;
---
+++ screens/ItemScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ScrollView, Text } from 'react-native';
+
+const ItemScreen: React.FC<{}> = () => {
+    return (
+        <ScrollView>
+            <Text>Item Screen</Text>
+        </ScrollView>
+    );
+}
+
+export default ItemScreen;
